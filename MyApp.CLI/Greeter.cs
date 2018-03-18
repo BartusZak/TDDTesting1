c5ea60752b06527beedb7646679f7ed92b61de44
BartusZak <b.ploszynski@gmail.com>
2018-03-18 17:56:56 +0100

    REfactor

--- MyApp.CLI/Greeter.cs
+++ MyApp.CLI/Greeter.cs
@@ -6,10 +6,6 @@ namespace MyApp.CLI
 {
     public class Greeter
     {
-        public string SayHello(string name)
-        {
-            return string.Format("Hello, {0}!", name);
-            throw new NotImplementedException();
-        }
+        public string SayHello(string name) => $"Hello, {name}!";
     }
 }
